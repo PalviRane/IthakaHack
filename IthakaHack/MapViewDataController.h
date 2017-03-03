d8c0698bc8fd425fc7cf763968f89017a8e65359
Potter <Palvi@aakashs-macbook-air.local>
2017-03-03 14:33:36 +0530

    Initial Commit

---
+++ IthakaHack/MapViewDataController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewDataController.h
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MapViewDataController : NSObject
+
+@end
---
+++ IthakaHack/MapViewDataController.m
@@ -0,0 +1,13 @@
+//
+//  MapViewDataController.m
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import "MapViewDataController.h"
+
+@implementation MapViewDataController
+
+@end
