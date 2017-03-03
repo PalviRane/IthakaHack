38a6b3d1dcba019e0553a0650c5715fa6977f309
Potter <Palvi@Aakashs-MacBook-Air.local>
2017-03-03 19:23:41 +0530

    pods commit

---
+++ IthakaHack/OfferTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  OfferTableViewCell.h
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OfferTableViewCell : UITableViewCell
+
+@end
---
+++ IthakaHack/OfferTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  OfferTableViewCell.m
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import "OfferTableViewCell.h"
+
+@implementation OfferTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
