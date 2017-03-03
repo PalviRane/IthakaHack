365ef9ee431c69bc2743abd8d46599024071e184
Potter <Palvi@aakashs-macbook-air.local>
2017-03-03 14:44:05 +0530

    MKMap View UI Commit

---
+++ IthakaHack/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IthakaHack/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
