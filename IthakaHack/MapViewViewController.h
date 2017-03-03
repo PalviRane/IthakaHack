b3cbb60e97fd8413be4bdf7de30b1d8ac749a7ec
Potter <Palvi@aakashs-macbook-air.local>
2017-03-03 14:24:24 +0530

    Initial Commit

---
+++ IthakaHack/MapViewViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewViewController.h
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewViewController : UIViewController
+
+@end
---
+++ IthakaHack/MapViewViewController.m
@@ -0,0 +1,37 @@
+//
+//  MapViewViewController.m
+//  IthakaHack
+//
+//  Created by Potter on 03/03/17.
+//  Copyright © 2017 PalviRane. All rights reserved.
+//
+
+#import "MapViewViewController.h"
+
+@interface MapViewViewController ()
+
+@end
+
+@implementation MapViewViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
