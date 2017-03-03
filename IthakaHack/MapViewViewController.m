4055019779ce8f953b7c880fc2a6ddfb1996a99a
Potter <Palvi@aakashs-macbook-air.local>
2017-03-03 16:18:34 +0530

    Annotations Commit

--- IthakaHack/MapViewViewController.m
+++ IthakaHack/MapViewViewController.m
@@ -32,6 +32,7 @@
 
 - (void)mapViewWillStartLoadingMap:(MKMapView *)mapView
 {
+    //Setting Thailand as Default Zoom
     CLLocationCoordinate2D thailandCoordinates;
     thailandCoordinates.latitude = 13.8700;
     thailandCoordinates.longitude = 100.9925;
@@ -40,8 +41,39 @@
     
     region.center = thailandCoordinates;
     
+    [self createAnnotationsWithLatitude:13.724561 andLongitude:100.4930249 withCityName:@"Bangkok"];
+    [self createAnnotationsWithLatitude:7.8833604 andLongitude:98.3744039 withCityName:@"Phuket"];
+    [self createAnnotationsWithLatitude:8.0306534 andLongitude:98.8174321 withCityName:@"Krabi"];
+    [self createAnnotationsWithLatitude:7.7526506 andLongitude:98.7390835 withCityName:@"Koh Phi Phi"];
+    [self createAnnotationsWithLatitude:9.50108 andLongitude:99.931372 withCityName:@"Koh Samui"];
+    [self createAnnotationsWithLatitude:9.1546336 andLongitude:99.2639922 withCityName:@"Surat Thani"];
+    
     [mapView setRegion:region animated:YES];
     
+}
+
+-(void)createAnnotationsWithLatitude:(CLLocationDegrees)cityLatitude andLongitude:(CLLocationDegrees)cityLongitude withCityName:(NSString *)cityName
+{
+    CLLocationCoordinate2D cityCoordinate;
+    cityCoordinate.latitude = cityLatitude;
+    cityCoordinate.longitude = cityLongitude;
+    
+    MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+    [annotation setCoordinate:cityCoordinate];
+    [annotation setTitle:cityName];
+    [_mapView addAnnotation:annotation];
+}
+
+-(void)mapView:(MKMapView *)mapView didAddAnnotationViews:(NSArray<MKAnnotationView *> *)views
+{
+    NSLog(@"annotationsAdded");
+    /*
+     //Placemark
+     MKPointAnnotation *annotation = [[MKPointAnnotation alloc] init];
+     [annotation setCoordinate:location];
+     [annotation setTitle:@"Customer Location"]; //You can set the subtitle too
+     [_userLocationMapView addAnnotation:annotation];
+     */
 }
 
 @end
